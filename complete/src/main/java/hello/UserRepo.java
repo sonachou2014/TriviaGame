a49c16eec36c31613fd293d0677ae787c3ea14fb
Sonam Choudhary <sonachou2014@gmail.com>
2019-02-14 09:54:51 +0100

    added by sonam

---
+++ complete/src/main/java/hello/UserRepo.java
@@ -0,0 +1,35 @@
+package hello;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserRepo {
+
+    private List<User> users;
+
+    public UserRepo() {
+        users = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            users.add(new User("user"+i+"@gmail.com","passw"+i));
+        }
+
+    }
+
+    public String getUser(String userName, String password) {
+        String isUserValid = "userNotValid";
+        for (User user : users) {
+            if ((user.getUserId()).equals(userName) && (user.getPassword()).equals(password)) {
+               isUserValid = "userValid";
+               break;
+            }
+
+        }
+       return isUserValid;
+
+    }
+
+}
